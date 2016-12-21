46982519357e8f0ecaca3720c009149059fe3001
Stefan Janssen <stefan.m.janssen@gmail.com>
2016-12-21 12:10:41 -0800

    restored Qiyun's code

---
+++ microprot/tests/test_processing.py
@@ -0,0 +1,50 @@
+from unittest import TestCase, main
+from skbio.util import get_data_path
+from skbio import Sequence
+
+from microprot.scripts.processing import extract_sequences
+
+
+class ProcessingTests(TestCase):
+    def setUp(self):
+        self.input_faa = get_data_path('test_processing/input.faa')
+
+    def test_extract_sequences(self):
+        obs = extract_sequences(self.input_faa)
+        exp = []
+        x = Sequence('PIVQNLQGQMVHQAISPRTLNAWVKVVEEKAFSPEVIPMFSALSEGATPQDLNTML'
+                     'NTVGGHQAAMQMLKETINEEAAEWDRLHPVHAGPIEPGQMREPRGSDIAGTTSTLQ'
+                     'EQIGWMTHNPPIPVGEIYKRWIILGLNKIVRMYSPTSILDIRQGPKEPFRDYVDRF'
+                     'YKTLRAEQASQEVKNWMTETLLVQNANPDCKTILKALGPAATLEEMMTACQGVGGP'
+                     'GHKARVL')
+        x.metadata['id'] = '3J4F_A'
+        x.metadata['description'] = ('Chain A, Structure Of Hiv-1 Capsid Prote'
+                                     'in By Cryo-em')
+        exp.append(x)
+        x = Sequence('MIQRTPKIQVYSRHPAENGKSNFLNCYVSGFHPSDIEVDLLKNGERIEKVEHSDLS'
+                     'FSKDWSFYLLYYTEFTPTEKDEYACRVNHVTLSQPKIVKWDRDM')
+        x.metadata['id'] = '1K5N_B'
+        x.metadata['description'] = ('Chain B, Hla-B2709 Bound To Nona-Peptide'
+                                     ' M9')
+        exp.append(x)
+        x = Sequence('KVFGRCELAAAMKRHGLDNYRGYSLGNWVCAAKFESNFNTQATNRNTDGSTDYGIL'
+                     'QINSRWWCNDGRTPGSRNLCNIPCSALLSSDITASVNCAKKIVSDGNGMNAWVAWR'
+                     'NRCKGTDVQAWIRGCRL')
+        x.metadata['id'] = '2VB1_A'
+        x.metadata['description'] = ('Chain A, Hewl At 0.65 Angstrom Resolutio'
+                                     'n')
+        exp.append(x)
+        self.assertEqual(obs, exp)
+        obs = extract_sequences(self.input_faa, seqidx=2)
+        exp = []
+        x = Sequence('MIQRTPKIQVYSRHPAENGKSNFLNCYVSGFHPSDIEVDLLKNGERIEKVEHSDLS'
+                     'FSKDWSFYLLYYTEFTPTEKDEYACRVNHVTLSQPKIVKWDRDM')
+        x.metadata['id'] = '1K5N_B'
+        x.metadata['description'] = ('Chain B, Hla-B2709 Bound To Nona-Peptide'
+                                     ' M9')
+        exp.append(x)
+        self.assertEqual(obs, exp)
+
+
+if __name__ == '__main__':
+    main()
