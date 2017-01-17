778b6010a4153b20d1652f683c0454e0c54698bd
qiyunzhu <qiyunzhu@gmail.com>
2017-01-16 16:02:10 -0800

    added processing functions

--- microprot/scripts/processing.py
+++ microprot/scripts/processing.py
@@ -1,45 +1,157 @@
+import os
+import sys
+import urllib
 import click
-
+from xml.dom import minidom
 from skbio import io
 
+"""
+Notes:
+This script has two functions:
+1. Extract select protein(s) from a multi-sequence Fasta file:
+    python processing.py -i input.faa -d 10 -o output.faa
+    python processing.py -i input.faa -d 1K5N.B -o output.faa
+    python processing.py -i input.faa -d 1,2,3 -o output.faa
+    python processing.py -i input.faa -d list.txt -o output.faa
+2. Extract representative proteins (local or remote) from PDB:
+    python processing.py -i pdb_seqres.txt -r represents.xml -o output.faa
+Or:
+    python processing.py -i pdb_seqres.txt -r 100 output.faa
+Stats:
+    pdb_seqres.txt has 381126 sequences
+    representatives?cluster=100 has 70132 protein IDs, as of Jan. 16, 2017.
+"""
 
-def extract_sequences(infile, seqidx=0):
+
+def extract_sequences(infile, identifiers=None):
     """ Extract sequence(s) from a multi-sequence FASTA file.
 
     Parameters
     ----------
     infile : str
         file path to input multi-sequence FASTA file
-    seqidx : int (optional)
-        n-th sequence of the input file to extract
-        (default: 0 for all sequences)
+    identifiers : str or list of str (optional)
+        IDs (names) or indexes (n-th sequence in the file) of the proteins to
+        extract, may be:
+        1) Python list
+        2) comma-separated list
+        3) file path to the list (one ID or index per line)
+        if omitted, all sequences will be extracted
 
-    Returns
-    -------
+    Return
+    ------
     list of skbio Sequence
+        extracted protein sequences
     """
+    l, ids, indexes = [], set(), set()
+    if identifiers:
+        if type(identifiers) is list:  # Python list
+            l = identifiers
+        elif os.path.isfile(identifiers):  # read from a local file
+            with open(identifiers, 'r') as f:
+                l = f.read().splitlines()
+        else:  # comma-separated list
+            l = list(map(str.strip, identifiers.split(',')))
+        for i in l:
+            if i.isdigit():
+                indexes.add(int(i))  # index of this protein in the file
+            else:
+                ids.add(i)  # protein ID (name)
     seqs = []
     iseq = 0  # current sequence index
-    for seq in io.read(infile, format='fasta'):
-        iseq += 1
-        if 0 < seqidx != iseq:
-            continue
-        seqs.append(seq)
+    for i, seq in enumerate(io.read(infile, format='fasta')):
+        if ids:
+            if seq.metadata['id'] in ids:
+                seqs.append(seq)
+        elif indexes:
+            if i+1 in indexes:  # indexes start with 1, not 0
+                seqs.append(seq)
+        else:
+            seqs.append(seq)
     return seqs
+
+
+def write_sequences(seqs, outfile):
+    """ Write sequence(s) into a multi-sequence FASTA file.
+
+    Parameters
+    ----------
+    seqs : str
+        list of skbio Sequence
+    outfile : str
+        file path to output multi-sequence FASTA file
+    """
+    def outseqs():
+        for seq in seqs:
+            yield seq
+    io.write(outseqs(), format='fasta', into=outfile)
+
+
+def read_representatives(represent):
+    """ Read representative protein IDs from file or server
+
+    Parameters
+    ----------
+    represent : str
+        file path to list of representative protein IDs in XML format, or an
+        integer representing the clustering level by which the list will be
+        downloaded from the PDB server
+
+    Return
+    ------
+    list of str
+        protein IDs
+
+    Raises
+    ------
+    ValueError
+        if neither repfile nor cluster is specified
+    """
+    ids = set()
+    xmldoc = None
+    if os.path.isfile(represent):  # read from a local file
+        xmldoc = minidom.parse(represent)
+    elif represent.isdigit():  # download from the PDB server
+        url_str = ('http://www.rcsb.org/pdb/rest/representatives?cluster=%s'
+                   % represent)
+        xml_str = urllib.request.urlopen(url_str).read()
+        xmldoc = minidom.parseString(xml_str)
+    else:
+        raise ValueError('Error: You must specify a local file path or the '
+                         'clustering level.')
+    for x in xmldoc.getElementsByTagName('pdbChain'):
+        l = x.attributes['name'].value.split('.')
+        ids.add('%s_%s' % (l[0].lower(), l[1]))
+    return sorted(list(ids))
 
 
 @click.command()
 @click.option('--infile', '-i', required=True,
-              type=click.Path(resolve_path=True, readable=True, exists=True,
-                              file_okay=True),
-              help='Input protein sequence file in FASTA format')
-@click.option('--seqidx', '-s', default=0,
-              help='Extract n-th sequence (default: all sequences)')
-def _main(infile, outfile, seqidx):
+              type=click.Path(resolve_path=True, readable=True, exists=True),
+              help='Input protein sequence file in FASTA format.')
+@click.option('--outfile', '-o', required=True,
+              type=click.Path(resolve_path=True, readable=True, exists=False),
+              help='Output protein sequence file in FASTA format.')
+@click.option('--identifiers', '-d', required=False,
+              help='Indexes or IDs of protein sequences to extract. May be a '
+              'comma-separated list, or an external file with one entry per '
+              'line. If not specified, all sequences will be extracted.')
+@click.option('--represent', '-r', required=False,
+              help='Generate representative protein sequences. One may either '
+              'provide a local protein ID list file (XML format), or let the '
+              'program download it from the PDB server by specifying a '
+              'desired clustering level.')
+def _processing(infile, outfile, identifiers, represent):
     """ Parsing arguments for processing
     """
-    extract_sequences(infile, seqidx)
-
+    if represent:
+        identifiers = read_representatives(represent)
+        print('Number of representative proteins: %s' % len(identifiers))
+    seqs = extract_sequences(infile, identifiers)
+    print('Number of extracted proteins: %s' % len(seqs))
+    if seqs:
+        write_sequences(seqs, outfile)
+    print('Task completed.')
 
 if __name__ == "__main__":
-    _main()
+    _processing()
--- microprot/tests/test_processing.py
+++ microprot/tests/test_processing.py
@@ -1,17 +1,30 @@
 from unittest import TestCase, main
+from shutil import rmtree
+from os import remove
+from os.path import join
+from tempfile import mkstemp, mkdtemp
 from skbio.util import get_data_path
 from skbio import Sequence
 
-from microprot.scripts.processing import extract_sequences
+from microprot.scripts.processing import (extract_sequences,
+                                          write_sequences,
+                                          read_representatives,
+                                          _processing)
 
 
 class ProcessingTests(TestCase):
     def setUp(self):
-        self.input_faa = get_data_path('test_processing/input.faa')
+        # temporary working directory
+        self.working_dir = mkdtemp()
 
-    def test_extract_sequences(self):
-        obs = extract_sequences(self.input_faa)
-        exp = []
+        # test data files
+        dir = 'test_processing'
+        self.input_faa = get_data_path(join(dir, 'input.faa'))
+        self.represent = get_data_path(join(dir, 'represent.xml'))
+        self.pdb_seqres = get_data_path(join(dir, 'pdb_seqres.txt'))
+
+        # test protein sequences
+        self.seqs = []
         x = Sequence('PIVQNLQGQMVHQAISPRTLNAWVKVVEEKAFSPEVIPMFSALSEGATPQDLNTML'
                      'NTVGGHQAAMQMLKETINEEAAEWDRLHPVHAGPIEPGQMREPRGSDIAGTTSTLQ'
                      'EQIGWMTHNPPIPVGEIYKRWIILGLNKIVRMYSPTSILDIRQGPKEPFRDYVDRF'
@@ -20,30 +33,70 @@ class ProcessingTests(TestCase):
         x.metadata['id'] = '3J4F_A'
         x.metadata['description'] = ('Chain A, Structure Of Hiv-1 Capsid Prote'
                                      'in By Cryo-em')
-        exp.append(x)
+        self.seqs.append(x)
         x = Sequence('MIQRTPKIQVYSRHPAENGKSNFLNCYVSGFHPSDIEVDLLKNGERIEKVEHSDLS'
                      'FSKDWSFYLLYYTEFTPTEKDEYACRVNHVTLSQPKIVKWDRDM')
         x.metadata['id'] = '1K5N_B'
         x.metadata['description'] = ('Chain B, Hla-B2709 Bound To Nona-Peptide'
                                      ' M9')
-        exp.append(x)
+        self.seqs.append(x)
         x = Sequence('KVFGRCELAAAMKRHGLDNYRGYSLGNWVCAAKFESNFNTQATNRNTDGSTDYGIL'
                      'QINSRWWCNDGRTPGSRNLCNIPCSALLSSDITASVNCAKKIVSDGNGMNAWVAWR'
                      'NRCKGTDVQAWIRGCRL')
         x.metadata['id'] = '2VB1_A'
         x.metadata['description'] = ('Chain A, Hewl At 0.65 Angstrom Resolutio'
                                      'n')
-        exp.append(x)
-        self.assertEqual(obs, exp)
-        obs = extract_sequences(self.input_faa, seqidx=2)
-        exp = []
-        x = Sequence('MIQRTPKIQVYSRHPAENGKSNFLNCYVSGFHPSDIEVDLLKNGERIEKVEHSDLS'
-                     'FSKDWSFYLLYYTEFTPTEKDEYACRVNHVTLSQPKIVKWDRDM')
-        x.metadata['id'] = '1K5N_B'
-        x.metadata['description'] = ('Chain B, Hla-B2709 Bound To Nona-Peptide'
-                                     ' M9')
-        exp.append(x)
-        self.assertEqual(obs, exp)
+        self.seqs.append(x)
+
+    def tearDown(self):
+        rmtree(self.working_dir)
+
+    def test_extract_sequences(self):
+        # extract all sequences
+        obs = extract_sequences(self.input_faa)
+        exp = self.seqs
+        self.assertListEqual(obs, exp)
+
+        # specify protein index
+        obs = extract_sequences(self.input_faa, identifiers='2')
+        exp = [self.seqs[1]]
+        self.assertListEqual(obs, exp)
+
+        # specify protein ID
+        obs = extract_sequences(self.input_faa, identifiers='1K5N_B')
+        exp = [self.seqs[1]]
+        self.assertListEqual(obs, exp)
+
+        # specify protein indexes using a comma-separated list
+        obs = extract_sequences(self.input_faa, identifiers='1,3')
+        exp = [self.seqs[0], self.seqs[2]]
+        self.assertListEqual(obs, exp)
+
+        # specify protein entries using an external file
+        listfile = join(self.working_dir, 'list.txt')
+        with open(listfile, 'w') as f:
+            f.write('%s\n%s\n' % ('1K5N_B', '2VB1_A'))
+        obs = extract_sequences(self.input_faa, identifiers=listfile)
+        remove(listfile)
+        exp = [self.seqs[1], self.seqs[2]]
+        self.assertListEqual(obs, exp)
+
+    def test_write_sequences(self):
+        seqs = extract_sequences(self.input_faa, identifiers='1')
+        outfile = join(self.working_dir, 'output.faa')
+        write_sequences(seqs, outfile)
+        with open(outfile, 'r') as f:
+            obs = f.read().splitlines()
+        remove(outfile)
+        exp = ['>%s %s' % (self.seqs[0].metadata['id'],
+                           self.seqs[0].metadata['description']),
+               str(self.seqs[0])]
+        self.assertListEqual(obs, exp)
+
+    def test_read_representatives(self):
+        obs = read_representatives(self.represent)
+        exp = ['1k5n_B', '2vb1_A', '3j4f_A']
+        self.assertListEqual(obs, exp)
 
 
 if __name__ == '__main__':
