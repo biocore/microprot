b55ff0145b2c37671196bbb05db959ab25ff34c2
tkosciol <tkosciolek@ucsd.edu>
2017-05-22 19:10:03 -0700

    snakemake little helpers

--- microprot/scripts/snakemake_helpers.py
+++ microprot/scripts/snakemake_helpers.py
@@ -22,36 +22,27 @@ def msa_size(msa_fp):
     msa_dir, msa_ext = os.path.splitext(os.path.abspath(msa_fp))
     if msa_ext is not '.a3m':
         msa_ext = '.a3m'
-    with(''.join([msa_dir, msa_ext]), 'r') as f:
+    with open(''.join([msa_dir, msa_ext]), 'r') as f:
         lines = f.readlines()
     msa_size = len([line for line in lines if line.startswith('>')])-1
     return msa_size
 
 
+# TODO
+# adding MSA size information omitted for now
 def append_db(fname, step=None, version=1, db_fp='/tmp/protein_db.index'):
     prots = process_fasta.extract_sequences(fname)
+    fp = trim(fname, '/')
     for prot in prots:
         prot_name = prot.metadata['id']
         timestamp = str(datetime.now())
-        msa_size = msa_size(fname)
+        # msa_fp = '%s/%s' % (fp, prot_name)
+        # msa_size = msa_size(msa_fp)
 
-        # > protein_name # source # commit_no # timestamp # MSA_size
+        # > protein_name # source # commit_no # timestamp
         append = '> %s # %s # %i # %s # %i\n' % (prot_name,
                                                  step,
                                                  version,
-                                                 timestamp,
-                                                 msa_size)
+                                                 timestamp)
         with open(db_fp, 'a') as f:
             f.write(append)
-
-
-def search_X(seq, split, output_base, shell_func):
-    for d_boundaries, domain in split_sequence(seq, split):
-        n = '%s-%s' % (str(d_boundaries[0]), str(d_boundaries[1]))
-        out = '%s_%s.%s' % (output_base, n, 'out')
-        a3m = '%s_%s.%s' % (output_base, n, 'a3m')
-        with open(output_base, 'a') as f:
-            f.write(out)
-            f.write(a3m)
-        shell_func
-        pass
