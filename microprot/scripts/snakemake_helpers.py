258d1bff02cedd4a8b0a4aaebe44d7f1be3a1c21
tkosciol <tkosciolek@ucsd.edu>
2017-07-01 16:45:09 -0700

    restructured Snakefile

--- microprot/scripts/snakemake_helpers.py
+++ microprot/scripts/snakemake_helpers.py
@@ -1,4 +1,5 @@
 import os
+import textwrap
 from datetime import datetime
 from microprot.scripts import process_fasta
 from skbio import io
@@ -29,14 +30,15 @@ def msa_size(msa_fp):
     return msa_size
 
 
-def parse_inputs(inp_file=None, inp_path=None, inp_from=None, inp_to=None,
+def parse_inputs(inp_fp=None, inp_from=None, inp_to=None,
                  microprot_inp=None, microprot_out=None):
-    SEQ_file = inp_file
-    SEQ_path = inp_path
-    SEQ_from = inp_from
-    SEQ_to = inp_to
-    SEQS = process_fasta.extract_sequences('%s/%s' % (SEQ_path, SEQ_file),
-                                           identifiers=(SEQ_from, SEQ_to))
+
+    for _dir in [microprot_inp, microprot_out]:
+        if not os.path.exists(_dir):
+            os.makedirs(_dir)
+
+    SEQS = process_fasta.extract_sequences(inp_fp,
+                                           identifiers=(inp_from, inp_to))
     SEQ_ids = []
     for SEQ in SEQS:
         _seq = SEQ.metadata['id']
@@ -49,16 +51,17 @@ def parse_inputs(inp_file=None, inp_path=None, inp_from=None, inp_to=None,
                                                             _seq))
         io.write(SEQ, format='fasta',
                  into='%s/%s' % (microprot_out, 'processed_sequences.fasta'))
-        return SEQ_ids
+    return SEQ_ids
+
 
 # TODO
 # adding MSA size information omitted for now
-def append_db(fname, step=None, version=1, db_fp='/tmp/protein_db.index'):
+def write_db(fname, step=None, version=1, db_fp='/tmp/protein_db'):
     prots = process_fasta.extract_sequences(fname)
     # fp = trim(fname, '/')
     for prot in prots:
         prot_name = prot.metadata['id']
-        timestamp = str(datetime.now())
+        timestamp = str(datetime.now()).split('.')[0]
         """
         `msa_fp` needs to be retrieved from 1 level up (e.g. 01 folder,
         instead of 02 folder. For future consideration. Added as issue #48
@@ -67,9 +70,19 @@ def append_db(fname, step=None, version=1, db_fp='/tmp/protein_db.index'):
         # msa_size = msa_size(msa_fp)
 
         # > protein_name # source # commit_no # timestamp # (msa_size)
-        append = '> %s # %s # %i # %s\n' % (prot_name,
-                                            step,
-                                            version,
-                                            timestamp)
+        append_idx = '>%s # %s # %i # %s\n' % (prot_name,
+                                               step,
+                                               version,
+                                               timestamp)
+        with open('%s.index' % db_fp, 'a') as f:
+            f.write(append_idx)
+
+        append_seq = '>%s\n%s\n' % (prot_name, textwrap.fill(str(prot[:]), 70))
         with open(db_fp, 'a') as f:
-            f.write(append)
+            f.write(append_seq)
+
+
+def append_db(source_root, dest_root):
+    for ext in ['', '.index']:
+        _dest = open('%s%s' % (dest_root, ext), 'a')
+        _dest.write(open('%s%s' % (source_root, ext), 'r').read())
